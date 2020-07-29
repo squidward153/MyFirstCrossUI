459829135265b1ea157faba6c38bcd53e42ace90
squidward153 <68975881+squidward153@users.noreply.github.com>
2020-07-29 19:45:54 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -1,47 +1,30 @@
-// The default code is a module class (inherited from xui.Module)
-// Ensure that all the value of "key/value pair" does not refer to external variables
 xui.Class('App', 'xui.Module',{
     Instance:{
-        // Dependency classes
         Dependencies:[],
-        // Required modules
         Required:[],
-
-        // To initialize properties
         properties : {},
-
-        // To initialize instance(e.g. properties)
         initialize : function(){
         },
-
-        // To initialize internal components (mostly UI controls)
-        // *** If you're not a skilled, dont modify this function manually ***
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label1")
+                .setLeft("20.571428571428573em")
+                .setTop("19.80952380952381em")
+                .setWidth("13.866666666666667em")
+                .setHeight("8.761904761904763em")
+                .setCaption("9+10=21")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
-
-        // Give a chance to determine which UI controls will be appended to parent container
         customAppend : function(parent, subId, left, top){
-            // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
         }
-        /*,
-        // To determine how properties affects this module
-        propSetAction : function(prop){
-        },
-        // To set all node's style in this modlue
-        customStyle:{}
-    },
-    //To customize the default properties and event handlers
-    Static:{
-        $DataModel:{
-        },
-        $EventHandlers:{
-        }
-    */
+
     }
 });
